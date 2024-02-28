6c77860b8c121f2b2ef0c734fc0b4f1c4b236816
Martin Hadley <161593295+iconicgauden@users.noreply.github.com>
2024-02-28 13:33:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,12 @@ int findFirst(string pattern, string text){
     }
     cout << "No Match Found!"<< endl;
     return -1;
-}
+    
+
+    }
+int levenshteinDistance(const std::string& str1, const std::string& str2) {
+    int m = str1.length();
+    int n = str2.length();}
 vector<int> findAll(string pattern, string text){
     int m = pattern.length(); 
     int n = text.length();
